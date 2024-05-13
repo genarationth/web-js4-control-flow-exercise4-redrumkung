8f3c4b88cf1cfc01b70cfcbc9d668b5d6ad50899
redrumkumg <n.ronnapat@github.com>
2024-05-13 17:23:19 +0700

    apply

---
+++ index.js
@@ -0,0 +1,19 @@
+const dayNumber = 8;
+const dayName =
+  dayNumber === 0
+    ? "Sunday"
+    : dayNumber === 1
+    ? "Monday"
+    : dayNumber === 2
+    ? "Tuesday"
+    : dayNumber === 3
+    ? "Wednesday"
+    : dayNumber === 4
+    ? "Thursday"
+    : dayNumber === 5
+    ? "Friday"
+    : dayNumber === 6
+    ? "Saturday"
+    : "Invalid day number";
+
+console.log(dayName);
